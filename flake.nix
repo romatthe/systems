62cd4c51d25d8cfe8b86639d2bc04ac8b87e0329
Robin Mattheussen <me@romatthe.dev>
2021-06-18 18:08:54 +0200

    Added emacs community overlay

--- flake.nix
+++ flake.nix
@@ -3,12 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     home-manager.url = "github:nix-community/home-manager/release-21.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }: 
+  outputs = { nixpkgs, home-manager, ... } @ inputs: 
     let
       system = "x86_64-linux";
 
@@ -19,6 +20,18 @@
 
       lib = nixpkgs.lib;
     in {
+
+      overlays = {
+        emacs = inputs.emacs-overlay;
+        unstable = final: prev: {
+          unstable = import inputs.nixpkgs-unstable {
+            inherit system;
+            config = {
+              allowUnfree = true;
+            };
+          };
+        };
+      };
 
       nixosConfigurations = {
         yokohama = lib.nixosSystem {
