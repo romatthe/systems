a05c50b47ea73c3625c79a760b5528842bcfb19d
Robin Mattheussen <me@romatthe.dev>
2021-06-18 19:21:31 +0200

    Configure primary monitor before starting xsession

--- machines/yokohama-home.nix
+++ machines/yokohama-home.nix
@@ -1,7 +1,11 @@
 { config, pkgs, lib, ... }:
 {
+  # Keyboard configuration
   home.keyboard = {
     layout = "us";
     variant = "qwerty";
   };
+
+  # Display configuration
+  xsession.profileExtra = "xrandr --output HDMI-1 --off --output DP-1 --primary --mode 3440x1440 --rate 100";
 }
