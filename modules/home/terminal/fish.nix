08fbf561590661a886af0baedf1344f4ba8515c7
Robin Mattheussen <me@romatthe.dev>
2021-06-21 01:08:54 +0200

    Tweaks to fish color scheme

--- modules/home/terminal/fish.nix
+++ modules/home/terminal/fish.nix
@@ -4,7 +4,7 @@
 
   # Shell aliases
   # Make sure to make the required utilities available!
-  programs.fish.shellAbbrs = {
+  programs.fish.shellAliases = {
     # Requires 'bat' to be installed
     "cat" = "bat";
     # Requires 'lsd' to be installed
@@ -25,6 +25,35 @@
     # Requires 'fd' to be installed
     "find" = "fd";
   };
+
+  programs.fish.shellInit = ''
+    set -U fish_color_cancel \x2dr
+    set -U fish_color_command 81a1c1
+    set -U fish_color_comment 434c5e
+    set -U fish_color_cwd green
+    set -U fish_color_cwd_root red
+    set -U fish_color_end 88c0d0
+    set -U fish_color_error ebcb8b
+    set -U fish_color_escape 00a6b2
+    set -U fish_color_history_current \x2d\x2dbold
+    set -U fish_color_host normal
+    set -U fish_color_host_remote yellow
+    set -U fish_color_match \x2d\x2dbackground\x3dbrblue
+    set -U fish_color_normal normal
+    set -U fish_color_operator 00a6b2
+    set -U fish_color_param eceff4
+    set -U fish_color_quote a3be8c
+    set -U fish_color_redirection b48ead
+    set -U fish_color_search_match bryellow\x1e\x2d\x2dbackground\x3dbrblack
+    set -U fish_color_selection white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack
+    set -U fish_color_status red
+    set -U fish_color_user brgreen
+    set -U fish_color_valid_path \x2d\x2dunderline
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description B3A06D\x1eyellow
+    set -U fish_pager_color_prefix white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
+    set -U fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
+  '';
 
   programs.fzf = {
     enable = true;
