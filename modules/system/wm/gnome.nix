27f27b2150dd356d6e4a0c30ea7ef51e40669307
Robin Mattheussen <me@romatthe.dev>
2021-06-21 04:29:26 +0200

    Attempt to launch Gnome under Wayland

--- modules/system/wm/gnome.nix
+++ modules/system/wm/gnome.nix
@@ -5,5 +5,5 @@
   # Enable the GNOME 3 Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  #services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.wayland = true;
 }
