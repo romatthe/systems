7f1493fb293f26b92b153d1b8d99f25263cf40b1
Robin Mattheussen <me@romatthe.dev>
2021-06-24 15:54:16 +0200

    Disable git ssh redirect

--- modules/terminal/git.nix
+++ modules/terminal/git.nix
@@ -13,9 +13,9 @@
 
     extraConfig = {
       # Always use SSH for Github repos
-      url."git@github.com:" = {
-        insteadOf = "https://github.com/";
-      };
+      #url."git@github.com:" = {
+      #  insteadOf = "https://github.com/";
+      #};
 
       # Use libsecret as the credential helper
       credential = {
