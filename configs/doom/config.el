b8fb5693c6eeab7a7ef561754fc7308fdcee3944
Robin Mattheussen <me@romatthe.dev>
2021-06-26 19:21:37 +0200

    Remove potential fix for nov-mode

--- configs/doom/config.el
+++ configs/doom/config.el
@@ -21,9 +21,6 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 13 :weight 'light)
-      doom-variable-pitch-font (font-spec :family "Noto Serif" :size 13)
-      ivy-posframe-font (font-spec :family "JetBrainsMono Nerd Font" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
