0f72da5e828d31e986685e1a7f64f292a6185be2
Robin Mattheussen <me@romatthe.dev>
2021-11-18 20:15:20 +0100

    Enable the use of the Tuxedo keyboard driver

--- machines/matsumoto.nix
+++ machines/matsumoto.nix
@@ -81,6 +81,9 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Tuxedo specific configurations
+  hardware.tuxedo-keyboard.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
