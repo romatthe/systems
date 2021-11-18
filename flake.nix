534ab23ed7174fb1ecc2212ed482d60e19c53da8
Robin Mattheussen <me@romatthe.dev>
2021-11-18 15:07:17 +0100

    Added configuration for personal dev laptop

--- flake.nix
+++ flake.nix
@@ -129,6 +129,32 @@
             })
           ];
         };
+
+        # Personal dev laptop
+        matsumoto = nixpkgs.lib.nixosSystem {
+          inherit pkgs;
+          system = "x86_64-linux";
+
+          modules = modules-common ++ [
+            # System modules
+            ./machines/matsumoto.nix
+            ./modules/apps/games.nix
+            ./modules/hardware/ssd.nix
+            ./modules/services/clam.nix
+
+            # Home manager modules
+            home-manager.nixosModules.home-manager
+            ({
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.romatthe = { ... }: {
+                imports = modules-common-home ++ [
+                  ./machines/matsumoto-home.nix
+                ];
+              };
+            })
+          ];
+        };
       };
 
     };
