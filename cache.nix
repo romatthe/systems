e20eac762c504495be55580f35fcf02a692a56c2
Robin Mattheussen <me@romatthe.dev>
2022-04-29 23:39:10 +0200

    Cleanup binary cache config (for real this time)

---
+++ cache.nix
@@ -0,0 +1,15 @@
+{
+  nix = {
+    trustedBinaryCaches = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    binaryCaches = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
