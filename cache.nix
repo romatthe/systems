1df365f40b467d249018b706efbf169967e1b110
Robin Mattheussen <me@romatthe.dev>
2022-04-30 00:03:10 +0200

    Add nixpkgs-wayland binary cache

--- cache.nix
+++ cache.nix
@@ -1,15 +1,17 @@
+let
+  caches = [ 
+    "https://cache.nixos.org"
+    "https://nix-community.cachix.org"
+    "https://nixpkgs-wayland.cachix.org"
+  ];
+in
 {
   nix = {
-    trustedBinaryCaches = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-    binaryCaches = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
+    trustedBinaryCaches = caches;
+    binaryCaches = caches;
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
   };
 }
