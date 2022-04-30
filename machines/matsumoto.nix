46422f5a15bf021c0b81b0d431a667e53d9fc603
Robin Mattheussen <me@romatthe.dev>
2022-04-30 22:11:09 +0200

    Added usbutils and pciutils as base deps for all machines

--- machines/matsumoto.nix
+++ machines/matsumoto.nix
@@ -63,7 +63,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget vim git htop cmake
+    wget vim git htop cmake usbutils pciutils
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
