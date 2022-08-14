8474c411045df072b87b110ee3aa171c06aca939
Robin Mattheussen <me@romatthe.dev>
2022-08-15 00:59:07 +0200

    Update flake inputs

--- modules/apps/vscode.nix
+++ modules/apps/vscode.nix
@@ -8,7 +8,9 @@
       keybindings = builtins.fromJSON (builtins.readFile ../../configs/vscode/keybindings.json);
       userSettings = builtins.fromJSON (builtins.readFile ../../configs/vscode/settings.json);
       extensions = with pkgs.vscode-extensions; [
-
+        arrterian.nix-env-selector
+        jnoortheen.nix-ide
+        matklad.rust-analyzer
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
           name = "ayu";
@@ -17,34 +19,10 @@
           sha256 = "f8816a8169622abfaa8c12e6425cc5e385cd6cdec1af96b5d7dbfd5809d93aee";
         }
         {
-          name = "elixir-ls";
-          publisher = "JakeBecker";
-          version = "0.8.1";
-          sha256 = "2f5e5ffd484ac74438c0dc4f3dda6617384ca07b96dbb684a44411fdc3317ea2";
-        }
-        {
           name = "intellij-idea-keybindings";
           publisher = "k--kato";
           version = "1.4.5";
           sha256 = "dd04e5fce50f94b8fd5387253802043254bdc5d488c19a207fe56262fddd7c71";
-        }
-        {
-          name = "nix-ide";
-          publisher = "jnoortheen";
-          version = "0.1.18";
-          sha256 = "7669b1feefa14507d8fcc08869275c56d6d89dfe5c2c20d4c2bef985e43172ec";
-        }
-        {
-          name = "nix-env-selector";
-          publisher = "arrterian";
-          version = "1.0.7";
-          sha256 = "0e76885c9dbb6dca4eac8a75866ec372b948cc64a3a3845327d7c3ef6ba42a57";
-        }
-        {
-          name = "rust-analyzer";
-          publisher = "matklad";
-          version = "0.2.809";
-          sha256 = "cd9fb06e0f97f8e4b399bb7a03ed4ef84e79be3b81e1318026b59d27b535a3ce";
         }
         {
           name = "toml";
