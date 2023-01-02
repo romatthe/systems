c3e150280e68d9f1594e190a0bbb8f288d48614a
Robin Mattheussen <me@romatthe.dev>
2023-01-02 11:28:29 +0100

    vulkan: prefer RADV over AMDVLK

--- modules/hardware/amdgpu.nix
+++ modules/hardware/amdgpu.nix
@@ -1,14 +1,14 @@
 { pkgs, config, ... }:
 {
-  # Enable the proprietary AMD drivers
+  # Enable the AMD drivers
   services.xserver.videoDrivers = [ "amdgpu" ];
 
     # Enable Vulkan support
   hardware.opengl.enable = true;
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
-  # hardware.opengl.extraPackages = [ pkgs.mesa.drivers ];
 
-  hardware.opengl.extraPackages = [ pkgs.mesa.drivers pkgs.amdvlk ];
-  hardware.opengl.extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+  # Disable AMDVLK for now, as I use an RX 6800 XT, which seems to work very well with RADV
+  #hardware.opengl.extraPackages = [ pkgs.mesa.drivers pkgs.amdvlk ];
+  #hardware.opengl.extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
 }
