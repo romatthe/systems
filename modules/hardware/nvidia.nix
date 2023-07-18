7462db947a3c74c021c3a25ec0b6750a0e6d8f1e
Robin Mattheussen <me@romatthe.dev>
2023-07-18 16:53:51 +0200

    nvidia: switch to built-in nvidia-offload

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,14 +1,4 @@
 { pkgs, ... }:
-
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in
 {
   # Enable the proprietary Nvidia driver
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -17,11 +7,9 @@ in
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
 
-  # This script allows us to offload rendering on the discrete GPU
-  environment.systemPackages = [ nvidia-offload ];
-
   hardware.nvidia.prime = {
     offload.enable = true;
+    offload.enableOffloadCmd = true;
 
     # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
     intelBusId = "PCI:0:2:0";
