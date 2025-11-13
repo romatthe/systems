472aa9c211bd863583e49a21bde280de40f93613
Robin Mattheussen <me@romatthe.dev>
2025-11-13 21:24:18 +0100

    jetbrains: wrap IntelliJ and Rust Rover with Node on PATH

--- modules/apps/jetbrains.nix
+++ modules/apps/jetbrains.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
-{
+let 
+  # Some wrappers to include Node on the path of Jetbrains IDEs because Amazon Q 
+  # needs it for some reason
+  idea-ultimate = pkgs.unstable.jetbrains.idea-ultimate.overrideAttrs (old: {
+    postInstall = ''
+      wrapProgram $out/bin/idea-ultimate \
+        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.nodejs ]}
+    '';
+  });
+  rust-rover = pkgs.unstable.jetbrains.rust-rover.overrideAttrs (old: {
+    postInstall = ''
+      wrapProgram $out/bin/rust-rover \
+        --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.nodejs ]}
+    '';
+  });
+in {
   environment.systemPackages = with pkgs; [
     unstable.jetbrains.clion
-    unstable.jetbrains.idea-ultimate
+    idea-ultimate
     unstable.jetbrains.goland
     unstable.jetbrains.rider
-    unstable.jetbrains.rust-rover # Still in EAP at the moment
+    rust-rover
   ];
 }
